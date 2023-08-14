9182517ea8287ef6923b43337142637f0d465013
Nate <ngtadlock@gmail.com>
2023-08-14 15:28:34 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+console.log("Let's get this party started!");
+
+let $gifs = $("#gifs");
+
+$("form").on("submit", async function(event) {
+    event.preventDefault();
+    let searchTerm = $("#input").val();
+    $("#input").val("");
+    let response =  await axios.get("https://api.giphy.com/v1/gifs/search", {params: {q: `${searchTerm}`, api_key: "8GvwCeVbPAedXxIrRY7ITuLB7HNK71tI"}})
+    addGif(response.data.data);
+});
+
+function addGif(response) {
+    let resultsIdx = response.length;
+    let randomIndex = Math.floor(Math.random() * resultsIdx);
+    let $newDiv = $("<div>");
+    let $newGif = $("<img>", {
+        src: response[randomIndex].images.original.url
+    });
+    $newDiv.append($newGif);
+    $gifs.append($newDiv);
+    }
+
+
+
+
+
+// API Key = 8GvwCeVbPAedXxIrRY7ITuLB7HNK71tI
